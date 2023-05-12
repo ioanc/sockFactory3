f4537db992f476f85840664ee08254c9d1e987e8
ioanc <ioanc@live.com>
2023-05-12 10:56:34 +0200

    debug node pod yaml example

--- debug-node-pod.sh
+++ debug-node-pod.sh
@@ -1,5 +1,40 @@
 # pod name: debug-node-pingpong
-# image name: mcr.microsoft.com/oss/nginx/nginx:1.21.6
 # node name: aks-pingpong-22031471-vmss000000
+# image name: mcr.microsoft.com/oss/nginx/nginx:1.21.6
 
-kubectl run debug-node-pingpong --restart=Never -it -n kube-system --rm --image overriden --overrides '{"spec": {"hostPID": true,"hostNetwork": true, "nodeSelector": { "kubernetes.io/hostname": "aks-pingpong-22031471-vmss000000"}, "tolerations": [{"operator": "Exists"}],"containers": [{"name": "nsenter", "image": "mcr.microsoft.com/oss/nginx/nginx:1.21.6","command": ["nsenter", "--all", "--target=1", "--", "su", "-"], "stdin": true, "tty": true, "securityContext": { "privileged": true }}] } }'
+
+kubectl run debug-node-pingpong --restart=Never -it -n kube-system --rm --image overriden \
+--overrides '{
+        "spec": 
+        {
+            "hostPID": true,
+            "hostNetwork": true, 
+            "nodeSelector": 
+                {
+                    "kubernetes.io/hostname": "aks-pingpong-22031471-vmss000000"
+                }, 
+            "tolerations": 
+                [
+                    {
+                        "operator": "Exists"
+                    }
+                ],
+            "containers": 
+                [
+                    {
+                        "name": "nsenter", 
+                        "image": "mcr.microsoft.com/oss/nginx/nginx:1.21.6",
+                        "command": 
+                            [
+                                "nsenter", "--all", "--target=1", "--", "su", "-"
+                            ], 
+                        "stdin": true, 
+                        "tty": true, 
+                        "securityContext": 
+                            {
+                                "privileged": true
+                            }
+                    }
+                ]
+        } 
+    }'
